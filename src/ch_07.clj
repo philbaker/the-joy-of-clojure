41196b17441ca3e291d3b44d9548a5cb767ac9ba
philbaker <phil@philbaker.me>
2021-10-30 11:41:03 +0100

    Add A* algorithm

--- src/ch_07.clj
+++ src/ch_07.clj
@@ -567,6 +567,7 @@
 (neighbors 5 [0 0])
 ; => ([1 0] [0 1])
 
+; Estimate the straight-line remaining path cost
 (defn estimate-cost [step-cost-est size y x]
   (* step-cost-est
      (- (+ size size) y x 2)))
@@ -576,3 +577,114 @@
 
 (estimate-cost 900 5 4 4)
 ; => 0
+
+; Calculate the cost of the path traversed so far
+(defn path-cost [node-cost cheapest-nbr]
+  (+ node-cost
+     (or (:cost cheapest-nbr) 0)))
+
+(path-cost 900 {:cost 1})
+; => 901
+
+; Calculate the estimated total cost of the path
+(defn total-cost [newcost step-cost-est size y x]
+  (+ newcost
+     (estimate-cost step-cost-est size y x)))
+
+(total-cost 0 900 5 0 0)
+; => 7200
+
+(total-cost 1000 900 5 3 4)
+; => 1900
+
+(total-cost (path-cost 900 {:cost 1}) 900 5 3 4)
+; => 1801
+
+; Retrieve the minimum value based on a criteria function
+(defn min-by [f coll]
+  (when (seq coll)
+    (reduce (fn [min other]
+              (if (> (f min) (f other))
+                other
+                min))
+            coll)))
+
+(min-by :cost [{:cost 100} {:cost 36} {:cost 9}])
+
+; A* algorithm to navigate the world. Tail-recursive solution
+(defn astar [start-yx step-est cell-costs]
+  (let [size (count cell-costs)]
+    (loop [steps 0
+           routes (vec (replicate size (vec (replicate size nil))))
+           work-todo (sorted-set [0 start-yx])]
+      (if (empty? work-todo)
+        [(peek (peek routes)) :steps steps]
+        (let [[_ yx :as work-item] (first work-todo)
+              rest-work-todo (disj work-todo work-item)
+              nbr-yxs (neighbors size yx)
+              cheapest-nbr (min-by :cost
+                                   (keep #(get-in routes %)
+                                         nbr-yxs))
+              newcost (path-cost (get-in cell-costs yx)
+                                 cheapest-nbr)
+              oldcost (:cost (get-in routes yx))]
+          (if (and oldcost (>= newcost oldcost))
+            (recur (inc steps) routes rest-work-todo)
+            (recur (inc steps)
+                   (assoc-in routes yx
+                             {:cost newcost
+                              :yxs (conj (:yxs cheapest-nbr [])
+                                         yx)})
+
+                   (into rest-work-todo
+                         (map
+                          (fn [w]
+                            (let [[y x] w]
+                              [(total-cost newcost step-est size y x) w]))
+                          nbr-yxs)))))))))
+
+
+; The Z world
+(astar [0 0]
+       900
+       world)
+; => [{:cost 17,
+;     :yxs
+;     [[0 0] [0 1] [0 2] [0 3] [0 4] [1 4] [2 4]
+;      [2 3] [2 2] [2 1] [2 0] [3 0] [4 0] [4 1]
+;      [4 2] [4 3] [4 4]]}
+;    :steps 94]
+
+
+; Shrubbery world
+(astar [0 0]
+       900
+       [[ 1 1 1   2 1]
+        [ 1 1 1 999 1]
+        [ 1 1 1 999 1]
+        [ 1 1 1 999 1]
+        [ 1 1 1   1 1]])
+; => [{:cost 9,
+;     :yxs [[0 0] [0 1] [0 2]
+;           [1 2]
+;           [2 2]
+;           [3 2]
+;           [4 2] [4 3] [4 4]]}
+;    :steps 134]
+
+
+; Bunny world
+(astar [0 0]
+       900
+       [[ 1 1 1   2 1]
+        [ 1 1 1 999 1]
+        [ 1 1 1 999 1]
+        [ 1 1 1 999 1]
+        [ 1 1 1 666 1]])
+; => [{:cost 10,
+;     :yxs [[0 0] [0 1] [0 2] [0 3] [0 4]
+;           [1 4]
+;           [2 4]
+;           [3 4]
+;           [4 4]]}
+;    :steps 132]
