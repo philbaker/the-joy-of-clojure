4db3103f5b70cf00c73c6384b59493fbcc613df0
philbaker <phil@philbaker.me>
2021-10-31 20:26:06 +0000

    Contract macro

--- src/ch_08.clj
+++ src/ch_08.clj
@@ -230,3 +230,76 @@ x
 
 (awhen 1 (awhen 2 [it]))
 ; => [2]
+
+; Contract top-level macro
+(declare collect-bodies)
+
+(defmacro contract [name & forms]
+  (list* `fn name (collect-bodies forms)))
+
+
+(declare build-contract)
+
+(defn collect-bodies [forms]
+  (for [form (partition 3 forms)]
+    (build-contract form)))
+
+; Contract auxiliary function build-contract
+(defn build-contract [c]
+  (let [args (first c)]
+    (list
+     (into '[f] args)
+     (apply merge
+            (for [con (rest c)]
+              (cond (= (first con) 'require)
+                    (assoc {} :pre (vec (rest con)))
+                    (= (first con) 'ensure)
+                    (assoc {} :post (vec (rest con)))
+                    :else (throw (Exception.
+                                  (str "Unknown tag "
+                                       (first con)))))))
+     (list* 'f args))))
+
+; Composition of the contract function and the constrained function
+(def doubler-contract
+  (contract doubler
+            [x]
+            (require
+             (pos? x))
+            (ensure
+             (= (* 2 x) %))))
+
+(def times2 (partial doubler-contract #(* 2 %)))
+
+(times2 9)
+
+(def times3 (partial doubler-contract #(* 3 %)))
+
+(times3 9)
+; Unhandled java.lang.AssertionError
+; Assert failed: (= (* 2 x) %)
+
+; Contract for multiple-arity functions
+(def doubler-contract
+  (contract doubler-contract
+            [x]
+            (require
+             (pos? x))
+            (ensure
+             (= (* 2 x) %))
+            [x y]
+            (require
+             (pos? x)
+             (pos? y))
+            (ensure
+             (= (* 2 (+ x y)) %))))
+
+((partial doubler-contract #(* 2 (+ %1 %2))) 2 3)
+; => 10
+
+((partial doubler-contract #(+ %1 %1 %2 %2)) 2 3)
+; => 10
+
+((partial doubler-contract #(* 3 (+ %1 %2))) 2 3)
+; Unhandled java.lang.AssertionError
+; Assert failed: (= (* 2 (+ x y)) %)
