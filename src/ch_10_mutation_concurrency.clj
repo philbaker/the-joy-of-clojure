8f5de1cb02bbb47f1930a48d07ec88de5c51c292
philbaker <phil@philbaker.me>
2021-11-25 21:07:18 +0000

    concurrency - agents

--- src/ch_10_mutation_concurrency.clj
+++ src/ch_10_mutation_concurrency.clj
@@ -199,3 +199,138 @@
 
 (dosync (ref-set to-move '[[:K [2 1]] [:k [0 1]]]))
 ;; => [[:K [2 1]] [:k [0 1]]]
+
+(defn stress-ref [r]
+  (let [slow-tries (atom 0)]
+    (future
+      (dosync
+       (swap! slow-tries inc)
+       (Thread/sleep 200)
+       @r)
+      (println (format "r is: %s, history: %d, after: %d tries"
+                       @r (.getHistoryCount r) @slow-tries)))
+    (dotimes [i 500]
+      (Thread/sleep 10)
+      (dosync (alter r inc)))
+    :done))
+
+(stress-ref (ref 0))
+;; => :done
+;; r is: 500, history: 10, after: 27 tries
+
+(stress-ref (ref 0 :max-history 30))
+;; => :done
+;; r is: 409, history: 20, after: 21 tries
+
+(stress-ref (ref 0 :min-history 15 :max-history 30))
+;; => :done
+;; r is: 116, history: 20, after: 6 tries
+
+;; Agents
+;; An agent represents an identity, a specific thing whose value
+;; can change over time. Each agent has a queue to hold actions
+;; that need to be performed on its value, and each action produces
+;; a new value for the agent to hold and pass to the subsequent action.
+
+(def joy (agent []))
+
+(send joy conj "First edition")
+
+@joy
+;; => ["First edition"]
+
+(defn slow-conj [coll item]
+  (Thread/sleep 10000)
+  (conj coll item))
+
+(send joy slow-conj "Second edition")
+
+@joy
+;; => ["First edition"]
+;; => ["First edition" "Second edition"]
+
+;; Controlling I/O with an agent
+(def log-agent (agent 0))
+
+(defn do-log [msg-id message]
+  (println msg-id ":" message)
+  (inc msg-id))
+
+(defn do-log [msg-id message]
+  (println msg-id ":" message)
+  (inc msg-id))
+
+(defn do-step [channel message]
+  (Thread/sleep 1)
+  (send-off log-agent do-log (str channel message)))
+
+(defn three-step [channel]
+  (do-step channel " ready to begin (step 0)")
+  (do-step channel " warming up (step 1)")
+  (do-step channel " really going now (step 2)")
+  (do-step channel " done! (step 3)"))
+
+(defn all-together-now []
+  (dothreads! #(three-step "alpha"))
+  (dothreads! #(three-step "beta"))
+  (dothreads! #(three-step "omega")))
+
+(all-together-now)
+
+@log-agent
+
+(do-step "important: " "this must go out")
+
+(await log-agent)
+
+(send log-agent (fn [_] 1000))
+
+(do-step "epsilon " "near miss")
+
+(defn exercise-agents [send-fn]
+  (let [agents (map #(agent %) (range 10))]
+    (doseq [a agents]
+      (send-fn a (fn [_] (Thread/sleep 1000))))
+    (doseq [a agents]
+      (await a))))
+
+;; send is for actions that stay busy using the processor and not
+;; not blocking on I/O or other threads
+;; send-off is for actinos that might block, sleep, or otherwise
+;; tie up the thread
+
+(time (exercise-agents send-off))
+;; "Elapsed time: 1007.446785 msecs"
+
+(time (exercise-agents send))
+;; "Elapsed time: 2001.43404 msecs"
+
+(send log-agent (fn [] 2000)) ; incorrect
+
+@log-agent
+
+(agent-error log-agent);; => #error {
+;;     :cause "Wrong number of args (1) passed...
+
+(send log-agent (fn [_] 3000))
+
+@log-agent
+
+;; To get the agent back in working order, you need to restart it:
+(restart-agent log-agent 2500 :clear-actions true)
+;; => 2500
+
+(send-off log-agent do-log "The agent, it lives!")
+
+(defn handle-log-error [the-agent the-err]
+  (println "An action sent to the log-agent threw " the-err))
+
+(set-error-handler! log-agent handle-log-error)
+
+(set-error-mode! log-agent :continue)
+
+(send log-agent (fn [x] (/ x 0)))
+;; => exception
+
+(send-off log-agent do-log "Stayin' alive, stayin' alive...")
+;; 2501 : Stayin' alive, stayin' alive...
